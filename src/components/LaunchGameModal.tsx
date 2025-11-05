900b09d7862099186fccd4f36f3fe8eafe7e2c09
Simon Faure <faure.smh@gmail.com>
2025-11-05 13:34:22 +0100

    Updated LaunchGameModal.tsx

--- src/components/LaunchGameModal.tsx
+++ src/components/LaunchGameModal.tsx
@@ -163,12 +163,8 @@ export function LaunchGameModal({ isOpen, onClose, gameTitle, gameUniqid, gameTy
                 className="w-full px-4 py-2 bg-slate-800 border border-slate-700 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 required
               >
-                {  console.log('defaultPattern0');
-                }
-                {  console.log(defaultPattern);
-                }
+
                 {
-                
                   patternFolders.length === 0 ? (
                   <option value="">Loading patterns...</option>
                 ) : (

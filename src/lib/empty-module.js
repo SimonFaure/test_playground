40ebc33ddc6a241744b5563acec52c0a1b1f0622
Simon Faure <faure.smh@gmail.com>
2025-11-13 12:20:02 +0100

    Updated empty-module.js

--- src/lib/empty-module.js
+++ src/lib/empty-module.js
@@ -1,2 +1,30 @@
 export default {};
-export const SerialPort = class {};
+
+export const SerialPort = class {
+  constructor() {
+    this.isOpen = false;
+  }
+
+  on(event, callback) {
+    return this;
+  }
+
+  write(data, callback) {
+    if (callback) callback();
+    return this;
+  }
+
+  close(callback) {
+    if (callback) callback();
+    return this;
+  }
+
+  open(callback) {
+    if (callback) callback();
+    return this;
+  }
+
+  static async list() {
+    return [];
+  }
+};
